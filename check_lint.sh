fbe281afc54260cf8b54484812bc41b41056ee56
Tom Cuypers <tom.cuypers@kiswe.com>
2017-08-28 09:44:28 +0200

    Update check_lint

---
+++ check_lint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+mypy .
+python -m pylint -f parseable -d I0011,R0801,F0401,R0904,W0702,W0512,R0201,E1101,C0325,W0613,W0603,W0602 \
+    --rcfile=pylintrc *.py */*.py ../test/*.py
+
